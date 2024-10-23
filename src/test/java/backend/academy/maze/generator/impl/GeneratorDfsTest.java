f50e7ced0cf4fd6c089e51d1a27bcc6d108ed4bb
Лапшин Илья Романович <ilia.lapshin2016@yandex.ru>
2024-10-24 01:58:06 +0700

    test(generator): добавлен тест GeneratorDfsTest
    Добавлен тест GeneratorDfsTest для проверки корректности создания лабиринта

---
+++ src/test/java/backend/academy/maze/generator/impl/GeneratorDfsTest.java
@@ -0,0 +1,40 @@
+package backend.academy.maze.generator.impl;
+
+import backend.academy.maze.generator.GeneratorMaze;
+import backend.academy.maze.maze.Cell;
+import backend.academy.maze.maze.Maze;
+import java.security.SecureRandom;
+import java.util.Arrays;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Тесты класса GeneratorDfs")
+public class GeneratorDfsTest {
+
+    @DisplayName("Тест создания непустого лабиринта с корректными размерами")
+    @Test
+    void testGenerateMaze() {
+        GeneratorMaze generatorMaze = new GeneratorDfs(new SecureRandom(), 100, 100);
+
+        int expectedHeight = 10;
+        int expectedWidth = 15;
+
+        Maze maze = generatorMaze.generateMaze(expectedHeight, expectedWidth);
+
+        int actualHeight = maze.height() - 2;
+        int actualWidth = maze.width() - 2;
+
+        assertThat(actualHeight).isEqualTo(expectedHeight);
+        assertThat(actualWidth).isEqualTo(expectedWidth);
+        assertThat(maze.grid()).isNotNull();
+        assertThat(Arrays.stream(maze.grid())
+            .flatMap(Arrays::stream)
+            .allMatch(cell -> cell.type() == Cell.CellType.WALL))
+            .isFalse();
+        assertThat(Arrays.stream(maze.grid())
+            .flatMap(Arrays::stream)
+            .allMatch(cell -> cell.type() == Cell.CellType.WALL || cell.type() == Cell.CellType.PASSAGE))
+            .isTrue();
+    }
+}
